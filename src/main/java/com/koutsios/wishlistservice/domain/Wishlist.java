f4e1ef4cc513347a43e54e7b5817a39b59e0bf8d
KoutsiosP <pany.koutsios@fujitsu.com>
2020-07-22 10:07:35 +0100

    Amended wishlist id access level

--- src/main/java/com/koutsios/wishlistservice/domain/Wishlist.java
+++ src/main/java/com/koutsios/wishlistservice/domain/Wishlist.java
@@ -1,12 +1,17 @@
 package com.koutsios.wishlistservice.domain;
 
+import static lombok.AccessLevel.PRIVATE;
+
 import java.util.List;
 import java.util.Map;
 import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,6 +23,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Wishlist {
 
   @Id
+  @Setter(PRIVATE)
   private String id;
 
   @NotBlank(message = "Wishlist must belong to a valid user")
